d086ffdd764864dc0bed2344eec55e8872d9611a
Josh Forbes <jdforbes82@gmail.com>
2015-01-12 15:25:01 -0500

    Adding google analytics

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -19,4 +19,18 @@
 
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 {{ HTML::script('js/built.js') }}
+
+{{--Analytics--}}
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53184039-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+{{--Analytics End--}}
+
 </html>
